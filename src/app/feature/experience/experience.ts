d529e7ed9902f6a4bd555ad7c30791ff8e63f850
dulait <dd20211096@student.fon.bg.ac.rs>
2025-12-19 21:42:38 +0100

    feat: add experience (#2)

---
+++ src/app/feature/experience/experience.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'experience',
+  imports: [],
+  templateUrl: './experience.html',
+  styleUrl: './experience.css',
+})
+export class Experience {
+
+}
