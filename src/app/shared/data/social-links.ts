58faf5126ef7e28c8adf9ebfa430992c342a3dac
dulait <dd20211096@student.fon.bg.ac.rs>
2025-12-20 23:09:56 +0100

    feat: expose shared social links (#16)

---
+++ src/app/shared/data/social-links.ts
@@ -0,0 +1,16 @@
+import { SocialLink } from '../model';
+
+export const GITHUB_SOCIAL_LINK: SocialLink = {
+  platform: 'GitHub',
+  url: 'https://github.com/dulait',
+} as const;
+
+export const LINKEDIN_SOCIAL_LINK: SocialLink = {
+  platform: 'LinkedIn',
+  url: 'https://www.linkedin.com/in/dusan-draskovic/',
+} as const;
+
+export const EMAIL_SOCIAL_LINK: SocialLink = {
+  platform: 'Email',
+  url: 'mailto:draskovicdusan4@gmail.com',
+} as const;
