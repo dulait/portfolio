f29dd122c9113785e0dccea3280cdaa7e2d01b44
dulait <dd20211096@student.fon.bg.ac.rs>
2026-04-15 00:45:06 +0200

    feat: simplify footer to links and copyright

--- src/app/core/components/footer/footer.component.ts
+++ src/app/core/components/footer/footer.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import {
+  EMAIL_SOCIAL_LINK,
+  GITHUB_SOCIAL_LINK,
+  LINKEDIN_SOCIAL_LINK,
+} from '../../../shared/data/social-links';
 
 @Component({
   selector: 'pf-footer',
@@ -8,4 +13,8 @@ import { Component } from '@angular/core';
 })
 export class FooterComponent {
   currentYear = new Date().getFullYear();
+
+  readonly EMAIL_LINK = EMAIL_SOCIAL_LINK;
+  readonly GITHUB_LINK = GITHUB_SOCIAL_LINK;
+  readonly LINKEDIN_LINK = LINKEDIN_SOCIAL_LINK;
 }
